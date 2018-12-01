fd434545d4f54b71915f0265940397790caafab1
Bret Johnson <BA@UCODE-TPV-MANAGER.fios-router.home>
2018-12-01 12:35:47 -0800

    How far I got before austin came

--- app.js
+++ app.js
@@ -19,21 +19,32 @@ const argv =  yargs
 .alias('help', 'h')
 .argv;
 
-console.log(`Yargs Address Encoded:  ${JSON.stringify(encodeURI(argv.a))}`);
-var encodedAddress = encodeURI(argv.a);
+console.log(`Yargs Address Encoded:  ${JSON.stringify(encodeURI(argv.address))}`);
+var encodedAddress = encodeURI(argv.address);
 
 
 
 request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=xpWEulU4cMA4WETcC5V3681wz9TNJ96T&location=${encodedAddress}`,
+    // url: `http://www.mapquestapi.com/geocoding/v1/address?key=xpWEulU4cMA4WETcC5V3681wz9TNJ96T&location=${encodedAddress}`,
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=xpWEulU4cMA4WETcC5V3681wz9TNJ96T&location=**&*((`,
     json: true
 }, (error, response, body) => {
+
+    if(error){
+        console.log('Unable to connect to Mapquest Servers');
+    }
+    else if(response.headers["content-length"] === "0"){
+        // console.log(JSON.stringify(response.headers["content-length"]));
+        console.log("Your address is invalid");
+    }
+
     //body.results[0].locations[0].latLng.lat
     //body.results[0].locations[0].latLng.lng
 
     // console.log(body);
     // console.log(JSON.stringify(response, undefined, 2));
-    console.log(`Address: ${body.results[0].providedLocation.location}`);
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-    console.log(`Longitutde: ${body.results[0].locations[0].latLng.lng}`);
+    // console.log(JSON.stringify(error, undefined, 2));
+    // console.log(`Address: ${body.results[0].providedLocation.location}`);
+    // console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+    // console.log(`Longitutde: ${body.results[0].locations[0].latLng.lng}`);
 });
